caf25750ed9ed74e86dffab51244c67858edde30
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2026-01-13 07:13:56 -0300

    chore: add typescript intellisense for `import.meta.env`

---
+++ frontend/src/vite-env.d.ts
@@ -0,0 +1,9 @@
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  readonly VITE_GOOGLE_OAUTH_URL: string;
+  readonly VITE_GITHUB_OAUTH_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
