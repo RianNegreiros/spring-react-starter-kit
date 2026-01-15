d06b9c7061a5f3d70dfd08114f727642b0b78ccf
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2026-01-15 08:30:44 -0300

    feat: add verification code entity

---
+++ backend/src/main/java/br/com/riannegreiros/backend/users/repository/VerificationCodeRepository.java
@@ -0,0 +1,13 @@
+package br.com.riannegreiros.backend.users.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.riannegreiros.backend.users.VerificationCode;
+
+public interface VerificationCodeRepository extends JpaRepository<VerificationCode, Long> {
+    Optional<VerificationCode> findByEmailAndCode(String email, String code);
+
+    void deleteByEmail(String email);
+}
